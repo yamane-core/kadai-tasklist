f7c7784bb743892a7c6a466e7e2daaf2314ff87d
seiya.yamane <yamane@coresite.ne.jp>
2021-03-30 10:28:49 +0000

    task model

--- database/migrations/2021_03_30_185705_create_tasks_table.php
+++ database/migrations/2021_03_30_185705_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasklistTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateTasklistTable extends Migration
      */
     public function up()
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('content');    // contentカラム追加
+            $table->string('content');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ class CreateTasklistTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('tasks');
     }
 }
